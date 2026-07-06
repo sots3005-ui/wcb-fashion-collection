96482a4a00ed862a2026f492ce78a5faf8cd25e7
sots3005-ui <sots3005@icloud.com>
2026-07-06 12:26:50 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -126,12 +126,12 @@ fadeElements.forEach((fadeElement) => {
 const mainImage = document.querySelector('.gallery-image img');
 const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
 
-for(let i = 0; i < thumbImages.length; i++) {
-  thumbImages[i].addEventListener('mouseover', (event) => {
-    mainImage.src = event.target.src;
-    mainImage.animate({opacity: [0, 1]}, 500);
-  });
-}
+// for(let i = 0; i < thumbImages.length; i++) {
+//   thumbImages[i].addEventListener('mouseover', (event) => {
+//     mainImage.src = event.target.src;
+//     mainImage.animate({opacity: [0, 1]}, 500);
+//   });
+// }
 thumbImages.forEach((thumbImage)=>{
   thumbImage.addEventListener('mouseover', (event) => {
     mainImage.src = event.target.src;
